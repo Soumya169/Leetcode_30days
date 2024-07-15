77ec08163e89a4b1ed5b62e94471a766e422164d
Soumya Ranjan Maharana <99471313+Soumya169@users.noreply.github.com>
2024-07-15 20:22:54 +0530

    Update Merge two sorted list .cpp

--- Accenture/Merge two sorted list .cpp
+++ Accenture/Merge two sorted list .cpp
@@ -1,1 +1,38 @@
+link of leetcode problem : https://leetcode.com/problems/merge-two-sorted-lists/
 
+**************************************************************************************
+
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        while(list1!=NULL && list2!=NULL){
+
+            if(list1->val <= list2->val){
+                curr->next = list1;
+                list1=list1->next;
+            }else{
+                curr->next = list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
+
+        }
+        curr->next = list1!=NULL ? list1:list2;
+
+        return dummy->next;
+
+        
+    }
+};
