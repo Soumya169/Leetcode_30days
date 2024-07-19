4f6778564b2ee9d6e0d72342f467b5138a9c626a
Soumya Ranjan Maharana <99471313+Soumya169@users.noreply.github.com>
2024-07-19 08:07:47 +0530

    Create Running Sum in Id Array .cpp

---
+++ Accenture/Running Sum in Id Array .cpp
@@ -0,0 +1,16 @@
+Leetcode Problem link - https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+
+  
+*********************************************************************************************
+
+  class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+
+        for (int i=1; i<nums.size();i++)
+            nums[i] += nums[i-1];
+          return nums;  
+        
+    }
+};
