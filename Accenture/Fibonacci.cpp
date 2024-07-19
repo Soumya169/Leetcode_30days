abb3257a4c6ebf656be948d13f1efac8b299f552
Soumya Ranjan Maharana <99471313+Soumya169@users.noreply.github.com>
2024-07-20 00:11:38 +0530

    Create Fibonacci.cpp

---
+++ Accenture/Fibonacci.cpp
@@ -0,0 +1,25 @@
+Leetcode Problem link - https://leetcode.com/problems/fibonacci-number/
+
+
+
+
+******************************************************************************************************
+
+
+
+class Solution {
+public:
+    int fib(int n) {
+        int a=0 ,b=1 , count =2 , c;
+        if(n==0||n == 1) return n;
+        while(count<=n){
+            c=a+b;
+            a=b;
+            b=c;
+            count++;
+        }
+        return c;
+
+        }
+        
+    };
