04c18b0242fa7b22e6873fa8254a9a5032509f2f
Soumya Ranjan Maharana <99471313+Soumya169@users.noreply.github.com>
2024-07-19 23:53:40 +0530

    Create Longest Common Prifix .cpp

---
+++ Accenture/Longest Common Prifix .cpp
@@ -0,0 +1,21 @@
+Leetcode prblem link - https://leetcode.com/problems/longest-common-prefix/
+
+
+********************************************************************************
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        
+for (int i=0;i<strs[0].length();i++){
+    for(auto s: strs){
+        if(s[i]!=strs[0][i]){
+            return ans;
+        }
+    }
+    ans+=strs[0][i];
+}
+return ans;
+    }
+};
